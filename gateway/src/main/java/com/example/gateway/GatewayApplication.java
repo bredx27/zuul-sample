48c0123ada7cfe7f0a3e6cfc530d24b15f01139b
Sayan <bredx2000@gmail.com>
2017-07-17 10:06:34 +0200

    test commit

--- gateway/src/main/java/com/example/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/example/gateway/GatewayApplication.java
@@ -1,8 +1,12 @@
 package com.example.gateway;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableZuulProxy
+@EnableAutoConfiguration
 @SpringBootApplication
 public class GatewayApplication {
 
